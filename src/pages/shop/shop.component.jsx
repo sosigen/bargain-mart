403eec13991f1aeea1a7d120716e675ac20cc363
sosigen <56933255+sosigen@users.noreply.github.com>
2021-06-08 20:57:39 +0200

    shop page, collection preview, collection items

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import SHOP_DATA from './shop.data'
+import PreviewCollection from '../../components/collection-preview/collection-preview.component'
+
+export default class ShopPage extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+  render(){
+    const {collections} = this.state
+    return(
+      <div className="shop-page">
+        {collections.map(({id, ...otherCollectionProps})=> (
+          <PreviewCollection key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    )
+  }
+}
