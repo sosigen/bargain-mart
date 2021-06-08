ff6ffc48e939e586ea8c5b4fb1c70753ed764016
sosigen <56933255+sosigen@users.noreply.github.com>
2021-06-08 19:50:13 +0200

    added basic navigation to components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './menu-item.styles.scss'
+import { withRouter } from 'react-router-dom'
 
-const MenuItem = ({title, imageUrl, size}) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className='background-image' style={{
-    backgroundImage: `url(${imageUrl})`
-  }}/>
+      backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
@@ -13,4 +15,4 @@ const MenuItem = ({title, imageUrl, size}) => (
   </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
