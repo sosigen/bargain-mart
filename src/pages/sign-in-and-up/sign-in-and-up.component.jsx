563f47ee39ea37fa11e7fd490b58c28b9ff1aea3
sosigen <56933255+sosigen@users.noreply.github.com>
2021-06-09 15:10:10 +0200

    sign in form

---
+++ src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './sign-in-and-up.styles.scss'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+const signInAndUpPage = () => (
+  <div className="sign-in-and-up">
+     zaloguj zarejestruj 
+     <SignIn/>
+  </div>
+  
+)
+export default signInAndUpPage
