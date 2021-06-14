71d5dc09127baa6073745bf88dbbede5daf283a1
sosigen <56933255+sosigen@users.noreply.github.com>
2021-06-14 16:20:05 +0200

    stripe integration, title and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./stripe-button.styles.scss";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey =
+		"pk_test_51J2FKVGo3pNi5tJZNDxLQnLi9nNAPwoFiJxS2L30InqEIEv4uE1r46ZegrY0oChDKVORYwh5MwoVF0M9mhdjpCCw006LypVyWr";
+	const onToken = (token) => {
+		console.log(token);
+		alert("success!");
+	};
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="Bargain Mart"
+			billingAddress
+			shippingAddress
+			image="https://svgshare.com/i/CUz.svg"
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
