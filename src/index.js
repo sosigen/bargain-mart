cea04819d56e2e40e3d23352288591bb209b0b67
sosigen <56933255+sosigen@users.noreply.github.com>
2021-06-14 14:35:34 +0200

    shop data reducers and selectors, selector of single category, new flow of shop data

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-import {Provider} from 'react-redux'
-import store from './redux/store'
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<React.StrictMode>
+			<BrowserRouter>
+				<PersistGate persistor={persistor}>
+					<App />
+				</PersistGate>
+			</BrowserRouter>
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById("root")
 );
-
