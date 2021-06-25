3563a4a19c3a57422ab2d2233ed484a668b8cf86
Szymon Nowacki <szybon.nowacki@gmail.com>
2021-06-25 13:06:45 +0200

    client/server separation, express server to handle stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
 	const priceForStripe = price * 100;
 	const publishableKey =
 		"pk_test_51J2FKVGo3pNi5tJZNDxLQnLi9nNAPwoFiJxS2L30InqEIEv4uE1r46ZegrY0oChDKVORYwh5MwoVF0M9mhdjpCCw006LypVyWr";
+	const headers = {
+		"Access-Control-Allow-Origin": "*",
+	};
 	const onToken = (token) => {
-		console.log(token);
-		alert("success!");
+		axios({
+			url: "payment",
+			method: "POST",
+			data: {
+				amount: priceForStripe,
+				token: token,
+			},
+			headers: headers,
+		})
+			.then((response) => {
+				alert("succesful payment");
+			})
+			.catch((error) => {
+				console.log("Payment Error: ", error);
+				alert(
+					"There was an issue with your payment! Please make sure you use the provided credit card."
+				);
+			});
 	};
 	return (
 		<StripeCheckout
