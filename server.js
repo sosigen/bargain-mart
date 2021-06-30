7d6271591efc466e17666a1ce88c83b7cc015f54
Szymon Nowacki <szybon.nowacki@gmail.com>
2021-06-30 16:38:53 +0200

    adding https to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 const compression = require("compression");
+const enforce = require("enforce");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -12,11 +13,13 @@ const stripe = require("stripe")(
 );
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(compression());
 if (process.env.NODE_ENV === "production") {
+	app.use(enforce.HTTPS({ trustProtoHeader: true }));
 	app.use(express.static(path.join(__dirname, "client/build")));
 
 	app.get("*", function (req, res) {
